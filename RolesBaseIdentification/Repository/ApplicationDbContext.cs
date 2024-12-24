45c111ac8a89361063b5759a4b018649b91f1126
Manish <manish.nagarkoti@np.technofex.com>
2024-12-24 20:49:03 +0545

    Initial commit

---
+++ RolesBaseIdentification/Repository/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RolesBaseIdentification.Model.CoreModel;
+
+namespace RolesBaseIdentification.Repository
+{
+    public class ApplicationDbContext: IdentityDbContext<IdentityUser, IdentityRole, string>
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            AppContext.SetSwitch("Npgsql.DisableDateTimeInfinityConversions", true);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("DbContext");
+
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+        public DbSet<EEmployee> Employees { get; set; }
+    }
+}
