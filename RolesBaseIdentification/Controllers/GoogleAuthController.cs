a441d3e59aeb548ec27501ab619204284ed1bc80
Manish <manish.nagarkoti@np.technofex.com>
2024-12-24 21:08:57 +0545

    initial commit

---
+++ RolesBaseIdentification/Controllers/GoogleAuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RolesBaseIdentification.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class GoogleAuthController : ControllerBase
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+		private readonly IConfiguration _configuration;
+
+		public GoogleAuthController(UserManager<IdentityUser> userManager, IConfiguration configuration)
+		{
+			_userManager = userManager;
+			_configuration = configuration;
+		}
+
+		
+	}
+}
